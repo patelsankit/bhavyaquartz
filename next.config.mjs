49a8dba6e4f50166aa96586ddd1ed7707dd0b147
Viren Chudasama <chudasamaviren08@gmail.com>
2024-08-31 17:39:15 +0530

    about page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
+    images: {
+        domains: ['www.evetis.in'], // Add the external domain here
+      },
 };
 
 export default nextConfig;
+    
